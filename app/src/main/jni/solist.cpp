6230a652d3bc5a57fcdb314ec60f9b5b9a964608
LoveSy <shana@zju.edu.cn>
2021-03-22 10:36:07 +0800

    Detect soname

--- app/src/main/jni/solist.cpp
+++ app/src/main/jni/solist.cpp
@@ -43,18 +43,27 @@ namespace Solist {
                         solist_realpath_offset))->c_str();
             }
 
+            const char *get_soname() {
+                return get_soname_sym ? get_soname_sym(this) : *((const char**) (
+                        (uintptr_t) this +
+                        solist_realpath_offset - sizeof(void*)));
+            }
+
             static bool setup(const SandHook::ElfImg &linker) {
                 get_realpath_sym = reinterpret_cast<decltype(get_realpath_sym)>(linker.getSymbAddress(
                         "__dl__ZNK6soinfo12get_realpathEv"_ienc .c_str()));
+                get_soname_sym = reinterpret_cast<decltype(get_soname_sym)>(linker.getSymbAddress(
+                        "__dl__ZNK6soinfo10get_sonameEv"_ienc .c_str()));
                 for (size_t i = 0; i < 1024 / sizeof(void *); i++) {
                     if (*(void **) ((uintptr_t) solist + i * sizeof(void *)) == somain) {
                         solist_next_offset = i * sizeof(void *);
-                        return android_get_device_api_level() < 26 || get_realpath_sym != nullptr;
+                        return android_get_device_api_level() < 26 || (get_realpath_sym != nullptr && get_soname_sym !=
+                                                                                                              nullptr);
                     }
                 }
                 LOGW("%s", "failed to search next offset"_ienc .c_str());
                 // shortcut
-                return android_get_device_api_level() < 26 || get_realpath_sym != nullptr;
+                return android_get_device_api_level() < 26 || (get_realpath_sym != nullptr && get_soname_sym != nullptr);
             }
 
             static size_t solist_next_offset;
@@ -65,7 +74,8 @@ namespace Solist {
 #endif
 
             // since Android 8
-            static const char *(*get_realpath_sym)(soinfo *);
+            inline static const char *(*get_realpath_sym)(soinfo *) = nullptr;
+            inline static const char *(*get_soname_sym)(soinfo *) = nullptr;
         };
 
 #ifdef __LP64__
@@ -73,9 +83,6 @@ namespace Solist {
 #else
         size_t soinfo::solist_next_offset = 0xa4;
 #endif
-
-        // since Android 8
-        const char *(*soinfo::get_realpath_sym)(soinfo *) = nullptr;
 
         const auto initialized = []() {
             SandHook::ElfImg linker(GetLinkerPath().c_str());
@@ -103,11 +110,12 @@ namespace Solist {
         }
         auto list = linker_get_solist();
         for (const auto &soinfo : linker_get_solist()) {
-            const auto &real_path = soinfo->get_realpath();
-            LOGD("%s", real_path);
-            if (real_path && std::string_view(real_path).find(keyword) != std::string::npos) {
+            if (const auto &real_path = soinfo->get_realpath(); real_path && std::string_view(real_path).find(keyword) != std::string::npos) {
                 paths.emplace(real_path);
                 LOGE("%s%s", "Found Riru:"_ienc .c_str(), real_path);
+            } else if(const auto &soname = soinfo->get_soname(); soname && std::string_view(soname).find(keyword) != std::string::npos) {
+                paths.emplace(soname);
+                LOGE("%s%s", "Found Riru:"_ienc .c_str(), soname);
             }
         }
         return paths;
